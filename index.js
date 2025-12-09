976f198fe30820bd8adb1de3d6a1b27e80fb33e2
Grigorii Bushmanov <grigorii_bushmanov@inbox.ru>
2025-12-09 15:52:48 +0300

    ts part 1

--- index.js
+++ index.js
@@ -7,50 +7,98 @@ var differenceInMonths = require('date-fns/differenceInMonths');
 var startOfMonth = require('date-fns/startOfMonth');
 var endOfMonth = require('date-fns/endOfMonth');
 var addDays = require('date-fns/addDays');
+const xmlhttprequest = require("xmlhttprequest");
+const checkUrlType = require('check-url-type');
 
-var d = new Date();
-var curyear = d.getFullYear();
 
-function getMonth(startDateStr) {
-    switch (startDateStr.substr(5,2)) {
-    case '01':
-        return "January ";
-    case '02':
-        return "February ";
-    case '03':
-        return "March ";
-    case '04':
-        return "April ";
-    case '05':
-        return "May ";
-    case '06':
-        return "June ";
-    case '07':
-        return "July ";
-    case '08':
-        return "August ";
-    case '09':
-        return "September ";
-    case '10':
-        return "October ";
-    case '11':
-        return "November ";
-    case '12':
-        return "December ";
+function getNetworkIconClass(network: string) {
+    network = network.toLowerCase();
+    switch (network) {
+        // special cases
+        case "google-plus":
+        case "googleplus":
+            return "fab fa-google-plus";
+        case "flickr":
+        case "flicker":
+            return "fab fa-flickr";
+        case "dribbble":
+        case "dribble":
+            return "fab fa-dribbble";
+        case "codepen":
+            return "fab fa-codepen";
+        case "soundcloud":
+            return "fab fa-soundcloud";
+        case "reddit":
+            return "fab fa-reddit";
+        case "tumblr":
+        case "tumbler":
+            return "fab fa-tumblr";
+        case "stack-overflow":
+        case "stackoverflow":
+            return "fab fa-stack-overflow";
+        case "blog":
+        case "rss":
+            return "fas fa-rss";
+        case "gitlab":
+            return "fab fa-gitlab";
+        case "keybase":
+            return "fas fa-key";
+        default:
+            // try to automatically select the icon based on the name
+            return "fab fa-" + network;
     }
 }
 
-function getMimeType(url) {
-    global.XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    return require('check-url-type').get_type(url) ;
+function getMonth(startDateStr: string) {
+    switch (startDateStr.substr(5, 2)) {
+        case '01':
+            return "January ";
+        case '02':
+            return "February ";
+        case '03':
+            return "March ";
+        case '04':
+            return "April ";
+        case '05':
+            return "May ";
+        case '06':
+            return "June ";
+        case '07':
+            return "July ";
+        case '08':
+            return "August ";
+        case '09':
+            return "September ";
+        case '10':
+            return "October ";
+        case '11':
+            return "November ";
+        case '12':
+            return "December ";
+    }
 }
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+class SplittedDateMixin {
+    year?: string
+    day?: string
+    month?: ReturnType<typeof getMonth>
+    mix(date: string) {
+        this.year = (date || "").substr(0, 4);
+        this.day = (date || "").substr(8, 2);
+        this.month = getMonth(date || "");
+    }
 }
 
-// utils.js или прямо в модуле
-function hasNonEmptyItem(array, field = null) {
+function getMimeType(url: string) {
+    global.XMLHttpRequest = xmlhttprequest.XMLHttpRequest;
+    return checkUrlType.get_type(url);
+}
+
+function capitalizeFirstLetter(row: string) {
+    return row.charAt(0).toUpperCase() + row.slice(1);
+}
+
+function hasNonEmptyItem(array: unknown, field: string | null = null) {
     return Array.isArray(array) && array.length > 0 && (
         field === null
             ? true
@@ -58,208 +106,189 @@ function hasNonEmptyItem(array, field = null) {
     );
 }
 
-
-function render(resumeObject) {
-
-    resumeObject.basics.capitalName = _.upperCase(resumeObject.basics.name);
-    if(resumeObject.basics && resumeObject.basics.email) {
-        // TODO: check gravatar
-        resumeObject.basics.gravatar = 'https:' + gravatar.url(resumeObject.basics.email, {
-                        s: '200',
-                        r: 'pg',
-                        d: 'mm'
-                    });
-    }
-    if (resumeObject.basics.image || resumeObject.basics.gravatar) {
-        resumeObject.photoBool = true;
-        resumeObject.photo = resumeObject.basics.image ? resumeObject.basics.image : resumeObject.basics.gravatar;
-        resumeObject.photoType = getMimeType(resumeObject.photo) ;
-    }
-
-    _.each(resumeObject.basics.profiles, function(p){
-        if(p.iconClass) {
-            return;
-        }
-        switch(p.network.toLowerCase()) {
-            // special cases
-            case "google-plus":
-            case "googleplus":
-                p.iconClass = "fab fa-google-plus";
-                break;
-            case "flickr":
-            case "flicker":
-                p.iconClass = "fab fa-flickr";
-                break;
-            case "dribbble":
-            case "dribble":
-                p.iconClass = "fab fa-dribbble";
-                break;
-            case "codepen":
-                p.iconClass = "fab fa-codepen";
-                break;
-            case "soundcloud":
-                p.iconClass = "fab fa-soundcloud";
-                break;
-            case "reddit":
-                p.iconClass = "fab fa-reddit";
-                break;
-            case "tumblr":
-            case "tumbler":
-                p.iconClass = "fab fa-tumblr";
-                break;
-            case "stack-overflow":
-            case "stackoverflow":
-                p.iconClass = "fab fa-stack-overflow";
-                break;
-            case "blog":
-            case "rss":
-                p.iconClass = "fas fa-rss";
-                break;
-            case "gitlab":
-                p.iconClass = "fab fa-gitlab";
-                break;
-            case "keybase":
-                p.iconClass = "fas fa-key";
-                break;
-            default:
-                // try to automatically select the icon based on the name
-                p.iconClass = "fab fa-" + p.network.toLowerCase();
-        }
-
-        if (p.url) {
-            p.text = p.url;
+/** Проверка наличия данных */
+function handleExistance(obj: { [key: string]: unknown }, ...nestedFieldsList: Array<[string, string] | string>) {
+    for (let nestedFields of nestedFieldsList) {
+        let initialKey: string, args: Parameters<typeof hasNonEmptyItem>;
+        if (typeof nestedFields === 'string') {
+            initialKey = nestedFields;
+            args = [obj[nestedFields]];
         } else {
-            p.text = p.network + ": " + p.username;
+            initialKey = nestedFields[0];
+            args = [obj[initialKey], nestedFields[1]];
+        }
+        obj[initialKey + 'Bool'] = hasNonEmptyItem(...args);
+    }
+}
+
+function handleStringArrayExistance(obj: { [key: string]: unknown }, fieldName: string) {
+    const value = obj[fieldName];
+    if (Array.isArray(value) && value[0] && value[0] != "") {
+        obj['bool' + capitalizeFirstLetter(fieldName)] = true;
+    }
+}
+
+class SplittedDateRangeMixin {
+    startDateYear?: string
+    startDateMonth?: string | undefined
+    endDateYear?: string
+    endDateMonth?: string | undefined
+    mix(startDate?: string, endDate?: string) {
+        if (startDate) {
+            this.startDateYear = startDate.substr(0, 4);
+            this.startDateMonth = getMonth(startDate);
+        }
+        if (endDate) {
+            this.endDateYear = endDate.substr(0, 4);
+            this.endDateMonth = getMonth(endDate);
+            if (Number(this.endDateYear) > new Date().getFullYear()) {
+                this.endDateYear += " (expected)";
+            }
+        } else {
+            this.endDateYear = 'Present'
+        }
+    }
+}
+
+class ExperienceMixin {
+    experience?: string
+    mix(startDate: string, endDate?: string) {
+        if (startDate) {
+            const months = differenceInMonths(
+                addDays(endOfMonth(endDate ? new Date(endDate) : new Date()), 1),
+                startOfMonth(new Date(startDate))
+            );
+            this.experience = formatDuration(
+                {
+                    years: Math.floor(months / 12),
+                    months: months % 12,
+                },
+                {
+                    format: ["years", "months"],
+                }
+            );
+        }
+    }
+}
+
+function handleWorkplace(w: any) {
+    const { startDate, endDate } = w;
+    SplittedDateRangeMixin.prototype.mix.call(w, startDate, endDate);
+    handleStringArrayExistance(w, 'highlights');
+    handleStringArrayExistance(w, 'keywords');
+    ExperienceMixin.prototype.mix.call(w, startDate, endDate);
+}
+
+function handleEducation(e: any) {
+    const { startDate, endDate } = e;
+    SplittedDateRangeMixin.prototype.mix.call(e, startDate, endDate);
+    handleStringArrayExistance(e, 'keywords');
+    handleStringArrayExistance(e, 'courses');
+    if (!e.area || !e.studyType) {
+        e.educationDetail = (e.area == null ? '' : e.area) + (e.studyType == null ? '' : e.studyType);
+    } else {
+        e.educationDetail = e.area + ", " + e.studyType;
+    }
+}
+
+function handleGravatar(basics: any) {
+    if (!basics?.email) {
+        return;
+    }
+    // TODO: check gravatar
+    basics.gravatar = 'https:' + gravatar.url(basics.email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+    });
+}
+
+function handleImage(obj: any) {
+    const { basics } = obj;
+    handleGravatar(basics);
+    const photo = basics?.image || basics?.gravatar;
+    if (photo) {
+        obj.photo = photo;
+        obj.photoBool = true;
+        obj.photoType = getMimeType(photo);
+    }
+}
+
+function importFile(name: string) {
+    return fs.readFileSync(__dirname + "/" + name, "utf-8");
+}
+
+function render(resumeObject: any) {
+    const { basics } = resumeObject;
+
+    basics.capitalName = _.upperCase(basics.name);
+
+    handleImage(resumeObject);
+
+    _.each(basics.profiles, function (p: any) {
+        if (!p.iconClass) {
+            p.iconClass = getNetworkIconClass(p.network);
         }
     });
 
-    function handleWorkplace(w) {
-        const { startDate, endDate } = w;
-        if (startDate) {
-            w.startDateYear = (startDate || "").substr(0,4);
-            w.startDateMonth = getMonth(startDate || "");
-        }
-        if(endDate) {
-            w.endDateYear = (endDate || "").substr(0,4);
-            w.endDateMonth = getMonth(endDate || "");
-        } else {
-            w.endDateYear = 'Present'
-        }
-        function handleStringArray(obj, fieldName) {
-            if (obj[fieldName]) {
-                if (obj[fieldName][0]) {
-                    if (obj[fieldName][0] != "") {
-                        obj['bool' + capitalizeFirstLetter(fieldName)] = true;
-                    }
-                }
-            }
-        }
-        handleStringArray(w, 'highlights');
-        handleStringArray(w, 'keywords');
-        if (startDate) {
-          const months = differenceInMonths(
-            addDays(endOfMonth(new Date(endDate)), 1),
-            startOfMonth(new Date(startDate))
-          );
-          w.workExperience = formatDuration(
-            {
-              years: Math.floor(months / 12),
-              months: months % 12,
-            },
-            {
-              format: ["years", "months"],
-            }
-          );
-        }
-    }
-
+    handleExistance(
+        resumeObject,
+        ['skills', 'name'],
+        ['interests', 'name'],
+        ['languages', 'language'],
+        ['references', 'name'],
+        ['publications', 'name'],
+        ['awards', 'title'],
+        ['education', 'institution'],
+        ['projects', 'name'],
+        'work',
+        'volunteer'
+    );
 
     // Work
-    if (hasNonEmptyItem(resumeObject.work)) {
-        resumeObject.workBool = true;
+    if (resumeObject.workBool) {
         _.each(resumeObject.work, handleWorkplace);
     }
 
     // Volunteer
-    if (hasNonEmptyItem(resumeObject.volunteer)) {
-        resumeObject.volunteerBool = true;
+    if (resumeObject.volunteerBool) {
         _.each(resumeObject.volunteer, handleWorkplace);
     }
 
-    // Projects
-    resumeObject.projectsBool = hasNonEmptyItem(resumeObject.projects, 'name');
-
     // Education
-    if (hasNonEmptyItem(resumeObject.education, 'institution')) {
-        resumeObject.educationBool = true;
-        _.each(resumeObject.education, function(e){
-                handleWorkplace(e);
-                if( !e.area || !e.studyType ){
-                  e.educationDetail = (e.area == null ? '' : e.area) + (e.studyType == null ? '' : e.studyType);
-                } else {
-                  e.educationDetail = e.area + ", "+ e.studyType;
-                }
-                if (e.startDate) {
-                    e.startDateYear = e.startDate.substr(0,4);
-                    e.startDateMonth = getMonth(e.startDate || "");
-                } else {
-                    e.endDateMonth = "";
-                }
-                if (e.endDate) {
-                    e.endDateYear = e.endDate.substr(0,4);
-                    e.endDateMonth = getMonth(e.endDate || "")
-
-                    if (e.endDateYear > curyear) {
-                        e.endDateYear += " (expected)";
-                    }
-                } else {
-                    e.endDateYear = 'Present'
-                    e.endDateMonth = '';
-                }
-                e.educationCourses = hasNonEmptyItem(e.courses);
-        });
+    if (resumeObject.educationBool) {
+        _.each(resumeObject.education, handleEducation);
     }
 
     // Awards
-    if (hasNonEmptyItem(resumeObject.awards, 'title')) {
-        resumeObject.awardsBool = true;
-        _.each(resumeObject.awards, function(a){
-                a.year = (a.date || "").substr(0,4);
-                a.day = (a.date || "").substr(8,2);
-                a.month = getMonth(a.date || "");
-            });
+    if (resumeObject.awardsBool) {
+        _.each(resumeObject.awards, (a: any) => SplittedDateMixin.prototype.mix.call(a, a.date));
     }
 
     // Publications
-    if (hasNonEmptyItem(resumeObject.publications, 'name')) {
-        resumeObject.publicationsBool = true;
-        _.each(resumeObject.publications, function(a){
-                a.year = (a.releaseDate || "").substr(0,4);
-                a.day = (a.releaseDate || "").substr(8,2);
-                a.month = getMonth(a.releaseDate || "");
-            });
+    if (resumeObject.publicationsBool) {
+        _.each(resumeObject.publications, (a: any) => SplittedDateMixin.prototype.mix.call(a, a.releaseDate));
     }
 
-    // Skills, Interests, Languages, References — только проверка наличия
-    resumeObject.skillsBool = hasNonEmptyItem(resumeObject.skills, 'name');
-    resumeObject.interestsBool = hasNonEmptyItem(resumeObject.interests, 'name');
-    resumeObject.languagesBool = hasNonEmptyItem(resumeObject.languages, 'language');
-    resumeObject.referencesBool = hasNonEmptyItem(resumeObject.references, 'name');
 
     // === CSS & шаблон ===
-    resumeObject.bootstrap = fs.readFileSync(__dirname + "/bootstrap.min.css", "utf-8");
-    resumeObject.fontawesome = fs.readFileSync(__dirname + "/fontawesome.min.css", "utf-8");
-    resumeObject.normalize = fs.readFileSync(__dirname + "/normalize.css", "utf-8");
+    {
+        resumeObject.bootstrap = importFile("bootstrap.min.css");
+        resumeObject.fontawesome = importFile("fontawesome.min.css");
+        resumeObject.normalize = importFile("normalize.css");
 
-    resumeObject.stylecss = fs.readFileSync(__dirname + "/style.css", "utf-8");
-    resumeObject.printcss = fs.readFileSync(__dirname + "/print.css", "utf-8");
+        resumeObject.stylecss = importFile("style.css");
+        resumeObject.printcss = importFile("print.css");
+    }
 
-    const theme = fs.readFileSync(__dirname + '/resume.template.html', 'utf8');
-    
+    const theme = importFile("resume.template.html");
+
     const resumeHTML = Mustache.render(theme, resumeObject);
 
     return resumeHTML;
 }
+
+
 module.exports = {
     render: render,
     pdfRenderOptions: {
